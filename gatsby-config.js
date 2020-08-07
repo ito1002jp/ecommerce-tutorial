e792f8ea38c30af9c7dead4194c0cf6e49a145db
Ohkura Tessyuu <ito1002jp@gmail.com>
2020-08-07 18:54:29 +0900

    install contentful plugins
    npm i gatsby-source-contentful @contentful/rich-text-react-renderer @contentful/rich-text-types

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -30,5 +34,15 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+       spaceId: process.env.CONTENTFUL_ID,
+       accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+       },
+   },
   ],
 }
