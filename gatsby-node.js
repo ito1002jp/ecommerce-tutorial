6f99b96e94a92229934b10412896c112db784e6f
Ohkura Tessyuu <ito1002jp@gmail.com>
2020-08-08 16:14:15 +0900

    changed gatsby-node.js to create each item page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,29 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+// use gatsby create pages api and query for all product slugs
+exports.createPages = async ({ actions, graphql }) => {
+    const { createPage } = actions;
+    const { data } = await graphql(`
+      {
+        allBags: allContentfulFashionTwoBags {
+          nodes {
+            productSlug
+          }
+        }
+      }
+    `);
+
+    // create page for each product and list
+    // them all in /products/:productSlug 
+    data.allBags.nodes.forEach(item => {
+      createPage({
+        path: `products/${item.productSlug}`,
+        component: path.resolve('./src/templates/Bag.js'),
+        context: {
+           // Data passed to context is available
+           // in page queries as GraphQL variables.
+          slug: item.productSlug,
+        },
+      });
+    });
+};
