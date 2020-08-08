b18384755bc4f32c9b250a7b90e2784c61635a69
Ohkura Tessyuu <ito1002jp@gmail.com>
2020-08-08 17:01:12 +0900

    changed the style of the escription with documentToReactComponents

--- src/templates/bag.js
+++ src/templates/bag.js
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Layout from '../components/layout';
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
 
 // run template query
 export const query = graphql`
@@ -42,10 +43,45 @@ width: 30rem;
 height: 30rem;
 `;
 
+// Add in some styling
+const Bold = styled.span`
+ font-weight: bold;
+ color: red;
+`;
+
+const P = styled.p`
+ color: orangered;
+`;
+
+const StyledHyperLink = styled.span`
+ color: purple;
+ padding: 1px 2px;
+ background: orange;
+ cursor: pointer;
+`;
+
+// render the styling
+const RTFBold = ({ children }) => <Bold>{children}</Bold>;
+const Text = ({ children }) => <P>{children}</P>;
+const HyperLink = ({ children }) => (
+ <StyledHyperLink>{children}</StyledHyperLink>
+);
+
+
 const BagTemplate = ({ data: { item } }) => {
+// modifying the options
 const options = {
-  // options for rich text formating
-};
+   renderMark: {
+     [MARKS.BOLD]: text => <RTFBold>{text}</RTFBold>,
+   },
+
+   renderNode: {
+     [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+     [INLINES.HYPERLINK]: (node, children) => (
+       <HyperLink>{children}</HyperLink>
+     ),
+   },
+ };
 
 return (
   <Layout>
